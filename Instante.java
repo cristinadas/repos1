1d72fdf112afe0696ebb2800c0aa184cb167e1fb
cristinadas <dascalescu_anacri@yahoo.com>
2019-03-22 14:14:38 +0200

    On branch master

--- Instante.java
+++ Instante.java
@@ -3,11 +3,7 @@ package Aplicatii;
 
 public class Instante {
     public static void main(String[] args) {
-        Presedinte p=Presedinte.getPresedinte("Iohanis", "Klaus", "Romania");
-        Presedinte q=Presedinte.getPresedinte("Ion", "Klaus", "Romania");
-        System.out.println(p==q);
-        System.out.println(q);
-        System.out.println(p);
+           System.out.println("hello!");
     }
     
 }
